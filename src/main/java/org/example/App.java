955eadfaa22bf6f182f883b9e32b730aefe22719
“itcast” <hello@123.com>
2021-07-18 16:54:12 +0800

    第三次提交

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" ); System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
+
+
+
+
+
+
+
+
+
+
     }
 }
